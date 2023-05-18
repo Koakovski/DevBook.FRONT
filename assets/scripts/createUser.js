d9f62de2041b9c712f05a07536a6e3aeb6c237f7
Higor Koakovski Pereira <hkoakovskip@gmail.com>
2023-05-18 15:43:56 -0300

    feat: added jquery to userCreate file

---
+++ assets/scripts/createUser.js
@@ -0,0 +1,28 @@
+$("#create-user-form").on("submit", createUser);
+
+function createUser(event) {
+    event.preventDefault();
+
+    const data = getFormFieldsValues();
+
+    if (data.password != data.confirmPassword) {
+        alert("Senhas não coincidem!");
+        return;
+    }
+
+    $.ajax({
+        url: "api/users",
+        method: "POST",
+        data,
+    });
+}
+
+function getFormFieldsValues() {
+    const name = $("#name").val();
+    const email = $("#email").val();
+    const nickname = $("#nickName").val();
+    const password = $("#password").val();
+    const confirmPassword = $("#confirmPassword").val();
+
+    return { name, email, nickname, password, confirmPassword };
+}
