78548ac44fe3bcbd69f3416d8f8b658a463a6cd6
Higor Koakovski Pereira <hkoakovskip@gmail.com>
2023-05-22 15:47:46 -0300

    fix: fixed template errors

--- src/domain/models/publication_model.go
+++ src/domain/models/publication_model.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Publication struct {
-	ID        uint64    `json:"id,omitempty"`
+	Id        uint64    `json:"id,omitempty"`
 	Title     string    `json:"title,omitempty"`
 	Content   string    `json:"content,omitempty"`
 	AuthorId  uint64    `json:"authorId,omitempty"`
